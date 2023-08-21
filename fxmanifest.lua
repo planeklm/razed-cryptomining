4ee0d8b4d3e3a62ca9217d4051babd89498a9f6a
KLM <91488137+planeklm@users.noreply.github.com>
2023-08-22 00:59:30 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version "cerulean"
+game "gta5"
+
+description "Razed's Crypto Mining"
+
+author 'Razed Scripts'
+
+version '1.0'
+
+lua54 'yes'
+
+client_script 'client/client.lua'
+server_script 'server/server.lua'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'
+}
