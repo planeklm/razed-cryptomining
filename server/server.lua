ed7ae6d60c41fd39845282faba0d945fc75a1426
KLM <91488137+planeklm@users.noreply.github.com>
2023-08-22 15:32:38 +0100

    Added check oxmysql to see if player owns the right to use it, added price check, added ox_lib notifications and more stuff

--- server/server.lua
+++ server/server.lua
@@ -1,1 +1,49 @@
 local QBCore = exports['qb-core']:GetCoreObject()
+
+local function getData(citizenid)
+    local data = MySQL.Sync.prepare('SELECT * FROM cryptominers where citizenid = ?', { citizenid })
+    return data
+end
+
+RegisterNetEvent('razed-cryptomining:buyCryptoMiner', function()
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    local notif1 = {
+        title = 'Payment Success',
+        description = 'You have successfully purcashed the crypto miner in cash.',
+        type = 'success'
+    }
+    local notif2 = {
+        title = 'Payment Success',
+        description = 'You have successfully purcashed the crypto miner with your bank.',
+        type = 'success'
+    }
+    local notif3 = {
+        title = 'Payment Failed',
+        description = 'You have insuffient funds either in your bank or cash.',
+        type = 'failed'
+    }
+
+    if Player.PlayerData.money.cash >= Config.Price['Stage 1'] then
+        TriggerClientEvent("ox_lib:notify", src, notif1)
+        Player.Functions.RemoveMoney('cash', Config.Price['Stage 1'], "Bought Stage 1 Crypto Miner")
+        local id = MySQL.insert('INSERT INTO `cryptominers` (citizenid) VALUES (?)',
+            { Player.PlayerData.citizenid })
+        TriggerClientEvent('razed-cryptomining:client:addinfo', src, getData(Player.PlayerData.citizenid))
+    elseif Player.PlayerData.money.bank >= Config.Price['Stage 1'] then
+        TriggerClientEvent("ox_lib:notify", src, notif1)
+        Player.Functions.RemoveMoney('bank', Config.Price['Stage 1'], "Bought Stage 1 Crypto Miner")
+        local id = MySQL.insert('INSERT INTO `cryptominers` (citizenid) VALUES (?)',
+        { Player.PlayerData.citizenid })
+        TriggerClientEvent('razed-cryptomining:client:addinfo', src, getData(Player.PlayerData.citizenid))
+    else 
+        TriggerClientEvent("ox_lib:notify", src, notif3)
+    end
+end)
+
+RegisterNetEvent('razed-cryptomining:server:getinfo', function()
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    local data = getData(Player.PlayerData.citizenid)
+    TriggerClientEvent('razed-cryptomining:client:addinfo', src, data)
+end)
