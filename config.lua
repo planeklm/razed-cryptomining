64a627d4243563b498da6034f06ccb13df7d80da
KLM <91488137+planeklm@users.noreply.github.com>
2023-08-22 15:33:18 +0100

    Added price for the stage 1 miner, choice for ox or qb for target, and added props for testing

--- config.lua
+++ config.lua
@@ -1,1 +1,11 @@
 Config = {}
+
+Config.CryptoMinerProp = {
+    `prop_laptop_01a`
+}
+
+Config.Target = 'qb' -- 'qb' or 'ox' depending on the target you use
+
+Config.Price = {
+    ['Stage 1'] = 888000, -- Price to buy the crypto miner
+}
