b7e39e769b193c3c5d2c6c925fbb8e97b4995edd
KLM <91488137+planeklm@users.noreply.github.com>
2023-08-22 23:17:42 +0100

    Added crypto withdrawal fee as well as shown on menu.

--- config.lua
+++ config.lua
@@ -4,6 +4,8 @@ Config.CryptoMinerProp = {
     `prop_laptop_01a`
 }
 
+Config.CryptoWithdrawalFeeShown = '10' -- This is a percentage of the fee when withdrawing the crypto - e.g 0.90 = 10%
+Config.CryptoWithdrawalFee = '0.90' -- This is a percentage of the fee when withdrawing the crypto - e.g 0.90 = 10%
 Config.Target = 'qb' -- 'qb' or 'ox' depending on the target you use
 
 Config.Price = {
