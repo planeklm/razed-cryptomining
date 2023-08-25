b4cdfd728fa7370836834289dd9aa7f7c5673c31
KLM <91488137+planeklm@users.noreply.github.com>
2023-08-25 20:47:04 +0100

    Added balance row to sql.

--- cryptominer.sql
+++ cryptominer.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS `cryptominers` (
     `citizenid` varchar(50) NOT NULL,
     `card` varchar(50) NOT NULL,
+    `balance` double NOT NULL,
     PRIMARY KEY (`citizenid`)
 )
